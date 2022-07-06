0d3dca7bfd417828db530b16180eca70b5b5a70c
Raestkjoot <gunnarmagnum@gmail.com>
2022-07-07 00:29:22 +0100

    Added Turrets

---
+++ Source/FPS_CodingChallenge/Turret.cpp
@@ -0,0 +1,102 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Turret.h"
+#include "Components/CapsuleComponent.h"
+//#include "Projectile.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystem.h"
+#include "GameFramework/Character.h"
+
+// Sets default values
+ATurret::ATurret()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+}
+
+// Called when the game starts or when spawned
+void ATurret::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Get player character reference
+	PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATurret::CheckFireCondition, FireRate, true);
+}
+
+// Called every frame
+void ATurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (InFireRange())
+	{
+		RotateTurret(PlayerCharacter->GetActorLocation());
+	}
+}
+
+void ATurret::HandleDestruction()
+{
+	if (DeathParticles)
+		UGameplayStatics::SpawnEmitterAtLocation(this, DeathParticles, GetActorLocation(), GetActorRotation());
+	if (DeathSound)
+		UGameplayStatics::PlaySoundAtLocation(this, DeathSound, GetActorLocation());
+	if (DeathCameraShakeClass)
+		GetWorld()->GetFirstPlayerController()->ClientStartCameraShake(DeathCameraShakeClass);
+
+	Destroy();
+}
+
+void ATurret::RotateTurret(FVector LookAtTarget)
+{
+	FVector ToTarget = LookAtTarget - TurretMesh->GetComponentLocation();
+	FRotator LookAtRotation = FRotator(0.f, ToTarget.Rotation().Yaw, 0.f);
+	TurretMesh->SetWorldRotation(LookAtRotation);
+}
+
+void ATurret::Fire()
+{
+	FVector Location = ProjectileSpawnPoint->GetComponentLocation();
+	FRotator Rotation = ProjectileSpawnPoint->GetComponentRotation();
+
+	//AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	//Projectile->SetOwner(this);
+}
+
+void ATurret::CheckFireCondition()
+{
+	if (PlayerCharacter == nullptr)
+	{
+		return;
+	}
+
+	if (InFireRange())// && PlayerCharacter->bAlive)
+	{
+		Fire();
+	}
+}
+
+bool ATurret::InFireRange()
+{
+	if (PlayerCharacter)
+	{
+		float Distance = FVector::Dist(GetActorLocation(), PlayerCharacter->GetActorLocation());
+		if (Distance <= FireRange) return true;
+	}
+	
+	return false;
+}
---
+++ Source/FPS_CodingChallenge/Turret.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Turret.generated.h"
+
+UCLASS()
+class FPS_CODINGCHALLENGE_API ATurret : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATurret();
+
+	void HandleDestruction();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void RotateTurret(FVector LookAtTarget);
+
+	void Fire();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* ProjectileSpawnPoint;
+
+
+	//UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	//TSubclassOf<class AProjectile> ProjectileClass;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	class UParticleSystem* DeathParticles;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	class USoundBase* DeathSound;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TSubclassOf<class UCameraShakeBase> DeathCameraShakeClass;
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 800.f;
+
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+	bool InFireRange();
+
+	class ACharacter* PlayerCharacter;
+};
